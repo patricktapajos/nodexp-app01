9891dee63630d86f74b4986800afc10b23b60f17
patrick <patrick@lazarus>
2019-09-16 16:22:29 -0400

    Adicao de arquivo gitignore

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.set('view engine', 'njk')
+app.use(express.urlencoded({ extended: false }))
+
+const users = ['User 1', 'User 2', 'User 3']
+
+app.get('/', (req, res) => {
+  return res.render('list', { users })
+})
+
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  return res.redirect('/')
+})
+
+app.listen(3000)
